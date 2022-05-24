059d400e7a84fe148fd5016b5021480c9461e488
Amanda Dittami <adittami@gmail.com>
2022-05-23 22:14:45 -0500

    fixed steve aokie audio reference for experiment. removed koreographer asset

--- Assets/Scripts/ArtistNFTLoader.cs
+++ Assets/Scripts/ArtistNFTLoader.cs
@@ -1,31 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
-using Moralis.Web3Api.Models;
-using MoralisWeb3ApiSdk;
 
 public class ArtistNFTLoader : MonoBehaviour
 {
-
     public VideoPlayer videoPlayer;
     public string videoUrl = "https://arweave.net/Bp2FN9YxicoL3NKOdFn0NrE_e0uZwEkRtMa3oIBoYIo";
-    private AudioSource audioSource;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+       
         videoPlayer.url = videoUrl;
-        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
-        audioSource = GetComponent<AudioSource>();
-        videoPlayer.SetTargetAudioSource(0, audioSource);
-        videoPlayer.EnableAudioTrack(0, true);
-        
+        //videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        //videoPlayer.EnableAudioTrack(0, true);        
         videoPlayer.Prepare();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
